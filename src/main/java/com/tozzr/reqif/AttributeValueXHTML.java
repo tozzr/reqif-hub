ecff2763eda9795dba318f179d6c02f81cf8c3e6
Torsten Hein <t.hein@vision-values.de>
2016-08-06 10:53:19 +0200

    spec objects complete

---
+++ src/main/java/com/tozzr/reqif/AttributeValueXHTML.java
@@ -0,0 +1,75 @@
+package com.tozzr.reqif;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class AttributeValueXHTML extends AttributeValue {
+
+	protected AttributeValueXHTML() {
+		super("XHTML");
+	}
+
+	@Override
+	public String toXml(int indent) {
+		return String.format(
+			"%s<ATTRIBUTE-VALUE-XHTML>\n"
+		  + "%s<DEFINITION>\n"
+		  + "%s<ATTRIBUTE-DEFINITION-XHTML-REF>%s</ATTRIBUTE-DEFINITION-XHTML-REF>\n"
+		  + "%s</DEFINITION>\n"
+		  + "%s<THE-VALUE>\n"
+		  + "%s%s\n"
+		  + "%s</THE-VALUE>\n"
+		  + "%s</ATTRIBUTE-VALUE-XHTML>\n",
+		  getIndentStr(indent),
+		  getIndentStr(indent+2),
+		  getIndentStr(indent+4),
+		  ref,
+		  getIndentStr(indent+2),
+		  getIndentStr(indent+2),
+		  getIndentStr(indent+4),
+		  value,
+		  getIndentStr(indent+2),
+		  getIndentStr(indent)
+		);
+	}
+	
+	public static AttributeValue fromElement(Element e) {
+		AttributeValue v = new AttributeValueXHTML();
+		v.handleElement(e);
+		return v;
+	}
+
+	@Override
+	protected void handleElement(Element e) {
+		if (e.getNodeName().equals("ATTRIBUTE-DEFINITION-XHTML-REF"))
+			ref = e.getTextContent();
+		if (e.getNodeName().equals("THE-VALUE"))
+			value = parseXHTML(e);
+		else
+			fromXml(e);
+	}
+	
+	private String parseXHTML(Node e) {
+		String xhtml = "";
+		NodeList children = e.getChildNodes();
+		for (int i=0; i<children.getLength(); i++) {
+			Node node = children.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				String name = node.getNodeName();
+				String attrStr = "";
+				if (node.hasAttributes()) {
+					NamedNodeMap attrs = node.getAttributes();
+					for (int j=0; j<attrs.getLength(); j++) {
+						Node a = attrs.item(j);
+						attrStr += String.format(" %s=\"%s\"", a.getNodeName(), a.getNodeValue()); 
+					}
+				}
+				xhtml += String.format("<%s%s>%s</%s>", name, attrStr, parseXHTML(node), name);
+			}
+		}
+		return xhtml.equals("") ? e.getTextContent() : xhtml;
+	}
+
+}
--- src/main/java/com/tozzr/reqif/SpecObject.java
+++ src/main/java/com/tozzr/reqif/SpecObject.java
@@ -1,21 +1,43 @@
 package com.tozzr.reqif;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.w3c.dom.Element;
 
 public class SpecObject extends Identifiable {
 
+	List<AttributeValue> values = new ArrayList<AttributeValue>();
+	String ref = "";
+	
 	protected SpecObject() {
 		super("SPEC-OBJECT");
 	}
 
 	@Override
 	public String toXml(int indent) {
+		String elemStr = "";
+		for (AttributeValue v : values)
+			elemStr += v.toXml(indent+4);
 		return String.format(
 			"%s<SPEC-OBJECT IDENTIFIER=\"%s\" LAST-CHANGE=\"%s\">\n"
+		  + "%s<VALUES>\n"
+		  + "%s"
+		  + "%s</VALUES>\n"
+		  + "%s<TYPE>\n"
+		  + "%s<SPEC-OBJECT-TYPE-REF>%s</SPEC-OBJECT-TYPE-REF>\n"
+		  + "%s</TYPE>\n"
 		  + "%s</SPEC-OBJECT>\n",
 			getIndentStr(indent),
 			identifier,
 			lastChange,
+			getIndentStr(indent+2),
+			elemStr,
+			getIndentStr(indent+2),
+			getIndentStr(indent+2),
+			getIndentStr(indent+4),
+			ref,
+			getIndentStr(indent+2),
 			getIndentStr(indent)
 		);
 	}
@@ -24,6 +46,24 @@ public class SpecObject extends Identifiable {
 		SpecObject s = new SpecObject();
 		s.handleElement(e);
 		return s;
+	}
+	
+	@Override
+	protected void handleElement(Element e) {
+		if (e.getNodeName().equals("SPEC-OBJECT")) {
+			super.handleElement(e);
+			fromXml(e);
+		}
+		if (e.getNodeName().equals("VALUES"))
+			fromXml(e);
+		if (e.getNodeName().equals("ATTRIBUTE-VALUE-XHTML"))
+			values.add(AttributeValueXHTML.fromElement(e));
+		if (e.getNodeName().equals("ATTRIBUTE-VALUE-STRING"))
+			values.add(AttributeValueString.fromElement(e));
+		if (e.getNodeName().equals("TYPE"))
+			fromXml(e);
+		if (e.getNodeName().equals("SPEC-OBJECT-TYPE-REF"))
+			ref = e.getTextContent();
 	}
 
 }
